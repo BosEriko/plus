a2eac9672315aec76aa8dbf18883d1f6e9df3d7d
BosEriko <bos@eriko.ph>
2025-06-01 18:57:00 +0800

    Feature: Revert Firebase Config in Favor of Next Fix

--- src/app/_utilities/env.js
+++ src/app/_utilities/env.js
@@ -2,7 +2,16 @@
 // More Information: https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#bundling-environment-variables-for-the-browser
 const env = {
   siteName: process.env.NEXT_PUBLIC_SITE_NAME,
-  twitchClientId: process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID
+  twitchClientId: process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID,
+  firebase: {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  },
 }
 
 export default env;
--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,6 @@
 import env from "./_utilities/env";
 
 export default function Home() {
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-}
-
-console.log('Firebase config:', firebaseConfig);
   const handleLogin = () => {
     const params = new URLSearchParams({
       client_id: env.twitchClientId,
