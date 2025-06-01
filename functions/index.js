fcc747698e12682580e1a5a8aa484015fe92afc0
BosEriko <bos@eriko.ph>
2025-06-01 16:27:53 +0800

    Feature: Remove Firebase Functions

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const twitch = require('./auth/twitch');
-const discord = require('./auth/discord');
-
-admin.initializeApp();
-
-exports.twitchCallback = twitch.twitchCallback(admin);
-exports.discordCallback = discord.discordCallback(admin);
