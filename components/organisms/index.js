4004b37108b7ea7c343ec785dc8e6026531a2123
BosEriko <bos@eriko.ph>
2025-08-22 14:46:46 +0800

    Feature: Create Footer, Header and Navbar Organism

--- components/organisms/index.js
+++ components/organisms/index.js
@@ -1,7 +1,13 @@
 import Authenticate from "./Authenticate";
+import Footer from "./Footer";
+import Header from "./Header";
+import Navbar from "./Navbar";
 
 const Organism = {
   Authenticate,
+  Footer,
+  Header,
+  Navbar,
 }
 
 export default Organism;
